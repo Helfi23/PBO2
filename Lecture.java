e651c940554e27caa97bb1a59b7ce8ba7936aabf
Helfi23 <55681352+Helfi23@users.noreply.github.com>
2021-09-29 11:36:51 +0700

    Add files via upload

---
+++ Lecture.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Helfi Apriliyandi F
+ */
+public class Lecture {
+    public static void main(String[] args) {
+      
+        int number = 10;
+        
+        String jawab = number > 20? number + " Lebih Besar Dari 20": number + " Lebih Kecil Dari 20";
+        
+        System.out.println(jawab);
+    }
+   
+}
---
+++ PBO2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2;
+
+/**
+ *
+ * @author Helfi Apriliyandi F
+ */
+public class PBO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String Hasil;
+        int Num1 = 50000;
+        int Num2 = 55000;
+        
+        Hasil = Num1 > Num2 ? "True":"False";
+         
+        System.out.println(Hasil);
+        
+    } 
+}
